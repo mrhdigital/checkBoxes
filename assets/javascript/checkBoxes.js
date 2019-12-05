906c2c42e894ddf3bc3b7475d777a97831a9850f
unknown <mrhdigital@yahoo.com>
2019-12-04 21:03:20 -0600

    fucntion handleCheck(e) works

--- assets/javascript/checkBoxes.js
+++ assets/javascript/checkBoxes.js
@@ -1,4 +1,10 @@
 //alert("js linked");
 
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
-console.log(checkboxes);
+//console.log(checkboxes);
+
+function handleCheck(e) {
+    console.log(e);
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
